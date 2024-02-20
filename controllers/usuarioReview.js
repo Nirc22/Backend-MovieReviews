311225fb93f27de4c625682f2d5ac61a866c78d0
Nicolas Rodriguez <nircandsim@gmail.com>
2024-02-19 23:31:15 -0500

    MovieReview OK - El usuario crea une review de una pelicula, la calificacion se almacena en moviereview, se actualiza el promedio, ademas, el usuario no puede crear mas de una review de una pelicula

--- controllers/usuarioReview.js
+++ controllers/usuarioReview.js
@@ -7,12 +7,12 @@ const Calificacion = require('../models/MovieReviews')
 
 const getReviewsById = async (req, resp = response) => {
     try {
-        
-        const {id} = req.params;
+
+        const { id } = req.params;
 
         console.log(id);
 
-        const listReviews = await UsuarioReview.find({"usuario": id});
+        const listReviews = await UsuarioReview.find({ "usuario": id });
         resp.status(200).json({
             ok: true,
             msg: 'Lista de reviews',
@@ -27,59 +27,41 @@ const getReviewsById = async (req, resp = response) => {
     }
 }
 
-const updateReview = async (pelicula, calificacion) =>{
+const updateMovieReview = async (pelicula, calificacion) => {
     
-    const listPelicula = await Calificacion.find({"pelicula": pelicula});
+    const listPelicula = await Calificacion.find({ "pelicula": pelicula });
     const peli = listPelicula[0];
-    
-
-    const review = peli.numReviews +1;   
-    const suma = peli.sumReviews +calificacion;
+    const review = peli.numReviews + 1;
+    const suma = peli.sumReviews + calificacion;
     const promedio = peli.calificacion = suma / review;
-
-    const movieReviewActualizado = await Calificacion.findByIdAndUpdate(peli.id, 
-                                                                        {numReviews: review, sumReviews: suma, calificacion: promedio}, { new: true });
-
-    
-
+    const movieReviewActualizado = await Calificacion.findByIdAndUpdate(peli.id,
+        { numReviews: review, sumReviews: suma, calificacion: promedio }, { new: true });
 }
 
 const crearUsuarioReview = async (req, resp = response) => {
-    try{
-        usuarioReview = new UsuarioReview(req.body);      
-        await usuarioReview.save();
-
-
-        // calificacion = new Calificacion();
-
-        const { pelicula , calificacion} = req.body;
-
-        updateReview( pelicula, calificacion)
-
+    try {
+        usuarioReview = new UsuarioReview(req.body);
+        const { usuario, pelicula, calificacion } = req.body;
+        const movie = await UsuarioReview.find({"pelicula": pelicula, "usuario": usuario });
+        if(movie.length === 0){
+            console.log(movie)
+            updateMovieReview(pelicula, calificacion)
+            await usuarioReview.save();
+            return resp.status(200).json({
+                ok: true,
+                msg: 'Review registrada correctamente',
+                review: usuarioReview
+                // review: reviewActualizada
+            });
+        }else{
+            return resp.status(400).json({
+                ok: false,
+                msg: 'No se puede, usuario ya tiene review para esta pelicula',
+                // review: reviewActualizada
+            });
+        }
         
-        // console.log(calificacion)
-
-        // const listPelicula = await Calificacion.find({"pelicula": pelicula});
-        // const peli = listPelicula[0];
-        
-
-        // const review = peli.numReviews +1;   
-        // const suma = peli.sumReviews +calificacion;
-        // const promedio = peli.calificacion = suma / review;
-
-        // const movieReviewActualizado = await Calificacion.findByIdAndUpdate(peli.id, 
-        //                                                                     {numReviews: review, sumReviews: suma, calificacion: promedio}, { new: true });
-
-
-
-        
-        return resp.status(200).json({
-            ok: true,
-            msg: 'Review registrada correctamente',
-            uid: usuarioReview.id,
-            // review: reviewActualizada
-        });
-    }catch(error) {
+    } catch (error) {
         console.log(error);
         return resp.status(500).json({
             ok: false,
@@ -92,30 +74,3 @@ module.exports = {
     crearUsuarioReview,
     getReviewsById
 }
-
-// const createReview = async(req, resp = response) => {
-//     const usuarioId = req.params.id;
-
-//     try {
-//         const usuario = await Usuario.findById(usuarioId);
-
-//         if(!usuario){
-//             resp.status(400).json({
-//                 ok:false,
-//                 mgs: 'El id del usuario no coincide con ningun elemento de la BD'
-//             });
-//         }
-//         const usuarioActualizado = await Usuario.findByIdAndUpdate(usuarioId, req.body, {new: true});
-//         resp.status(200).json({
-//             ok:true,
-//             msg: 'Review creada',
-//             review: usuarioActualizado
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         resp.status(400).json({
-//             ok: false,
-//             msg: "Error al crear la review"
-//         });
-//     }
-// }
